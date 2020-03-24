665991083f4472d65b2928c2f9ca21748935ee13
Maria Ayala <mayala@research.baycrest.org>
2020-03-24 08:53:40 -0400

    p2 part 2

--- practice1_births_part1_part2.R
+++ practice1_births_part1_part2.R
@@ -1,12 +1,19 @@
-# find the top state for births
+
 library(dplyr)
 yg <- read.csv("https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/06_Stats/US_Baby_Names/US_Baby_Names_right.csv",
                header=TRUE)
 head(yg)
 
+# find the top state for births
 yg.summary <- yg %>%
   group_by(State) %>%
   summarise(total_births = sum(Count)) %>%
   arrange(desc(total_births))
 
 print(yg.summary[1,])
+
+# determine what the top boy and girl names were in 2009
+yg.2009 <- yg %>%
+  filter(Year == 2009) %>%
+  group_by(Gender) %>%
+  top_n(1)
